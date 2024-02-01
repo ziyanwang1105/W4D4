93407c56e07fe3d08c5b55104e39d5a9c70545e0
Jerry Wang <ziyanwang1105@gmail.com>
2024-02-01 10:56:26 -0800

    finish project 1

--- practice-for-ch-ruby-execution-time-differences-long-practice-main/project_1.rb
+++ practice-for-ch-ruby-execution-time-differences-long-practice-main/project_1.rb
@@ -8,3 +8,30 @@ def my_min_2(arr)
 
     arr.inject(10000000){|acc, ele| ele < acc ? ele : acc}
 end
+
+
+def largest_contiguous_subsum(arr)
+
+    subarrays = []
+    (0...arr.length).each do |i|
+        subarray = []
+        (i...arr.length).each do |j|
+            subarray << arr[j]
+            subarrays << subarray.dup
+        end
+    end
+    subarrays.map{|ele| ele.reduce(:+)}.max
+end
+
+def lcs(arr)
+    largest = -1000000
+    current = 0
+    arr.each do |num|
+#kadane algorithm
+        current += num
+        largest = current if largest < current
+        current = 0 if current < 0
+    end
+
+    return largest
+end
